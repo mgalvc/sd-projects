f268abca4c6135ca2c1f244cbed19ce0b473d454
mgalvc <matheusgcorreia@gmail.com>
2018-10-24 15:43:43 -0300

    create instances of pll and ddio

--- Problema_1/TOP.v
+++ Problema_1/TOP.v
@@ -5,8 +5,14 @@ module TOP(
 	output wire lcd_en,
 	output wire lcd_rs,
 	output wire lcd_rw,
-	output wire [4:0] leds
+	output wire [4:0] leds,
+	input wire [7:0] datain_h,
+	input wire [7:0] datain_l
 );
+
+	wire [3:0] pll_clk;
+	wire [7:0] ddio_out;
+	wire locked;
 
 	nios nios2(
 		.buttons_connection_export(buttons[3:0]),
@@ -16,6 +22,23 @@ module TOP(
 		.leds_connection_export(leds[4:0]),
 		.rs_export(lcd_rs),
 		.rw_export(lcd_rw)
+	);
+	
+	my_pll pll(
+		.areset(reset),
+		.inclk0(clock),
+		.c0(pll_clk[0]),
+		.c1(pll_clk[1]),
+		.c2(pll_clk[2]),
+		.c3(pll_clk[3]),
+		.locked(locked)
+	);
+	
+	my_ddio_out ddio(
+		.datain_h(datain_h[7:0]),
+		.datain_l(datain_l[7:0]),
+		.outclock(clock),					// is that right?
+		.dataout(ddio_out[7:0])
 	);
 
 endmodule
